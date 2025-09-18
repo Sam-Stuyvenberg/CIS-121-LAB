9d313e5380e2a15125b75907a782a2682e3e34f1
Sam Stuyvenberg <115120797+Sam-Stuyvenberg@users.noreply.github.com>
2025-09-18 11:41:35 -0500

    update

--- Week 3/Lab3.py
+++ Week 3/Lab3.py
@@ -4,5 +4,9 @@
 number = int(input("Enter a number: "))
 if number > 0:
     print("I'ts Possitive.")
+elif number > 1000:
+    print("It's A large figure")
+elif number > 1000000:
+    print("It's A larger figure")
 else:
-    print("It's Negative")
+    print("It's a negative number")
